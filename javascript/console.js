80d2c19a42d0a67f2730eac23cf1d5913269690a
yeon-seong <dd300@naver.com>
2024-08-29 18:31:32 +0900

    :sparkles: 콘솔 메시지에 CSS 스타일 적용

---
+++ javascript/console.js
@@ -0,0 +1,20 @@
+console.error(
+  "%c🚨 THIS IS ERROR MESSAGE : 치명적인 페이지 오류!!!! 🚨",
+  "background: linear-gradient(red, crimson);color: pink; font-weight: bold; padding: 10px; border-radius: 40px;"
+);
+console.warn(
+  "%c🖐 THIS IS WARNING MESSAGE : 경고 발생! 주의하세요! 🖐",
+  "background: linear-gradient(gold, orange);color: lemonchiffon; font-weight: bold; padding: 10px; border-radius: 40px"
+);
+console.debug(
+  "%c✅ THIS IS DEBUG MESSAGE : 디버깅을 통한 버그 탐색 중 ✅",
+  "background: linear-gradient(chartreuse, green);color: mediumspringgreen; font-weight: bold; padding: 10px; border-radius: 40px"
+);
+console.info(
+  "%c🌌 THIS IS INFO MESSAGE : 도착한 정보를 확인해주세요. 🌌",
+  "background: linear-gradient(blue, midnightblue);color: skyblue; font-weight: bold; padding: 10px; border-radius: 40px"
+);
+console.log(
+  "%c🪀 THIS IS LOG MESSAGE : 콘솔 메시지에 스타일 입히기~ 🪀",
+  "background: linear-gradient(violet, purple);color: plum; font-weight: bold; padding: 10px; border-radius: 40px"
+);
--- js/console.js
+++
@@ -1,6 +0,0 @@
-/* 콘솔 메시지 */
-console.error("ERROR");
-console.warn("WARNING");
-console.debug("DEBUG");
-console.info("INFORMATION");
-console.log("LOG");
